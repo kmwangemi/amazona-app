d393e29f9dba51f9ecc2884161e2c7ca2a94f8ad
kmwangemi <mwangemik@gmail.com>
2021-12-02 10:40:32 +0300

    restructure app folder

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -2,9 +2,11 @@ import express from 'express';
 import expressAsyncHandler from 'express-async-handler'
 import data from '../data.js';
 import Product from '../models/productModel.js';
+import { isAdmin, isAuth} from '../utils.js';
 
 const productRouter = express.Router();
 
+// get all products
 productRouter.get('/', expressAsyncHandler(async (req, res) => {
    const products = await Product.find({});
    res.send(products)
@@ -16,6 +18,7 @@ productRouter.get('/seed', expressAsyncHandler(async (req, res) => {
    res.send({ createdProducts })
 }))
 
+// get product by id
 productRouter.get('/:id', expressAsyncHandler(async (req, res) => {
    const product = await Product.findById(req.params.id);
    if (product) {
@@ -23,6 +26,23 @@ productRouter.get('/:id', expressAsyncHandler(async (req, res) => {
    } else {
       res.status(404).send({message: 'Product Not Found'})
    }
+}))
+
+// create a product
+productRouter.post('/', isAuth, isAdmin, expressAsyncHandler(async (req, res) => {
+   const product = new Product({
+      name: 'sample name' + Date.now(),
+      image: './images/p1.jpg',
+      price: 0,
+      category: 'sample category',
+      brand: 'sample brand',
+      countInStock: 0,
+      rating: 0,
+      numReviews: 0,
+      description: 'sample description'
+   })
+   const createdProduct = await product.save()
+   res.status(200).send({ message: 'Product', product: createdProduct });
 }))
 
 export default productRouter;
